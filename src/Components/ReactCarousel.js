6280d316f217c05d882d08ae72a5cbbce86b2128
Harman <herman.muhemed@gmail.com>
2022-12-04 22:13:15 +0300

    feat:slider added

--- src/Components/ReactCarousel.js
+++ src/Components/ReactCarousel.js
@@ -2,13 +2,7 @@ import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
 
-import Slider1 from "../Assets/Images/1.png";
-import Slider2 from "../Assets/Images/2.png";
-import Slider3 from "../Assets/Images/3.png";
-import Slider4 from "../Assets/Images/4.png";
-import Slider5 from "../Assets/Images/5.png";
-import Slider6 from "../Assets/Images/6.png";
-import Slider7 from "../Assets/Images/7.png";
+import Slider1 from "../Assets/Images/1st.png";
 
 
 export default function ReactCarousel() {
@@ -18,25 +12,7 @@ export default function ReactCarousel() {
                 <div>
                     <img src={Slider1} alt="slider img" />
                 </div>
-                <div>
-                    <img src={Slider2} alt="slider img" />
-                </div>
-                <div>
-                    <img src={Slider3} alt="slider img" />
-                </div>
-                <div>
-                    <img src={Slider4} alt="slider img" />
-                </div>
-                <div>
-                    <img src={Slider5} alt="slider img" />
-                </div>
-                <div>
-                    <img src={Slider6} alt="slider img" />
-                </div>
-                <div>
-                    <img src={Slider7} alt="slider img" />
-                </div>
-            </Carousel>
+    </Carousel>
        
     </>
   )

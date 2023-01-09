8e141e2066b7dcefa5b6e9b16b7d9c3d195184c8
Harman <herman.muhemed@gmail.com>
2023-01-09 21:20:38 +0300

    hotfix:CallBtn place changed

--- src/Components/CallBtn.js
+++ src/Components/CallBtn.js
@@ -7,7 +7,7 @@ function CallBtn() {
   return (
     <>
       <Tippy content="Call Us!">      
-    <div className='fixed bottom-10 right-24 h-14 w-14 z-50'>
+    <div className='fixed bottom-28 right-5 h-14 w-14 z-50'>
     <a href="tel:+964-750-782-1514" > 
     <img src={CallIcon} alt="Call icon img" />
     </a>
